c7d0428e420194ebeda914467e476e5f12effd3a
maxdudko <maxdudko.dev.ua@gmail.com>
2025-12-30 21:17:50 +0200

    updated migrations

---
+++ apps/api/prisma/migrations/20251230174447_user_profile_add_fields/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `passwordHash` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "passwordHash" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "UserProfile" ADD COLUMN     "displayName" TEXT NOT NULL DEFAULT 'User';
