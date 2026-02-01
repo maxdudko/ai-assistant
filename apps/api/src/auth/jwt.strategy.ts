bc445cdde883738c156e88d5e9cfe6d2438098df
maxdudko <maxdudko.dev.ua@gmail.com>
2026-02-01 20:13:21 +0200

    fix

--- apps/api/src/auth/jwt.strategy.ts
+++ apps/api/src/auth/jwt.strategy.ts
@@ -15,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
           return null;
         },
       ]),
-      secretOrKey: 'dev-secret',
+      secretOrKey: process.env.JWT_SECRET || 'dev-secret',
     });
   }
 
