44de79d213a40982ab3257f86c05a64c56068ecd
maxdudko <maxdudko.dev.ua@gmail.com>
2026-02-14 15:40:40 +0200

    navbar update

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -12,12 +12,12 @@ const Navbar: FC = () => {
     <aside className="w-64 border-r border-neutral-800 p-4 flex flex-col justify-between">
       <nav className="flex flex-col gap-2">
         <Link href="/me/">Home</Link>
-        <Link href="/me/conversations">Conversations</Link>
         <Link href="/me/chat">Chat</Link>
-        <Link href="/me/goals">Goals</Link>
+        <Link href="/me/conversations">Conversations</Link>
         <Link href="/me/tasks">Tasks</Link>
-        <Link href="/me/memory">Memory</Link>
+        <Link href="/me/goals">Goals</Link>
         <Link href="/me/info-digests">Info Digests</Link>
+        <Link href="/me/memory">Memory</Link>
         <Link href="/me/logs">Logs</Link>
         <Link href="/me/profile">Profile</Link>
       </nav>
